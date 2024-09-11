3323b7163c60d1d8fd41a208fa9be985e4db5d1e
wsmcrn <zs1cyranik@gmail.com>
2024-09-11 20:11:52 +0200

    feat: add thumnails

--- sell_that_sheet/sell_that_sheet/services/directorybrowser.py
+++ sell_that_sheet/sell_that_sheet/services/directorybrowser.py
@@ -1,21 +1,40 @@
 import os
 from django.conf import settings
 
+IMAGES_EXTENSIONS = [".jpg", ".JPG", ".jpeg", ".JPEG", ".png", ".PNG", ".bmp", ".BMP", ".webp", ".WEBP"]
+
+def get_thumbnail_url(path, base_path):
+    # return url to thumbnail of the image
+    # replace double backslashes with single backslashes
+    # replace backslashes with forward slashes
+    url = "http://172.27.70.154/images/"
+    path = path.replace(base_path, '').replace("\\\\", '/').replace("\\", '/')
+    return f"{url}{path}"
+
 
 def list_directory_contents(path=None):
     base_path = os.environ.get("AUCTION_MEDIA_ROOT", settings.MEDIA_ROOT)
+    print(base_path)
     if path is not None:
         # Ensure that the path is safe and within the base path
         full_path = os.path.normpath(os.path.join(base_path, path))
+        print(full_path)
         if not full_path.startswith(base_path):
             raise ValueError("Attempted to access a path outside of the base path")
     else:
         full_path = base_path
 
+    double_backslash = '\\\\'
     try:
         with os.scandir(full_path) as it:
             entries = [
-                {"name": entry.name, "isDir": entry.is_dir(), "id": entry.name}
+                {
+                    "name": entry.name,
+                    "isDir": entry.is_dir(),
+                    "id": entry.name,
+                    # generate thumbnail url if the entry is a file and has an image extension and replace
+                    "thumbnailUrl": get_thumbnail_url(entry.path, base_path) if entry.is_file() and entry.name.endswith(tuple(IMAGES_EXTENSIONS)) else None,
+                }
                 for entry in it
             ]
         return entries
