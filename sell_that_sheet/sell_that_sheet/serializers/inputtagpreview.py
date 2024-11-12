f189aefd8a7350df1aea9f56ac4e73beaac72372
wsmcrn <zs1cyranik@gmail.com>
2024-11-12 20:31:08 +0100

    fix: add tag preview and preparation

--- sell_that_sheet/sell_that_sheet/serializers/inputtagpreview.py
+++ sell_that_sheet/sell_that_sheet/serializers/inputtagpreview.py
@@ -3,4 +3,4 @@ from rest_framework import serializers
 class InputTagField(serializers.Serializer):
     categoryId = serializers.IntegerField(required=True)
     auctionName = serializers.CharField(required=True)
-    auctionTags = serializers.CharField(required=False)
+    auctionTags = serializers.CharField(required=False, allow_null=True, allow_blank=True)
